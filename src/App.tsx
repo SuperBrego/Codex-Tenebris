c0496a60098e51aba0d36368dc0b448183693b4b
Bernardo <bernardo_rego@hotmail.com>
2025-04-12 17:02:59 -0300

    FIX: Colocado Theme Provier

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import EditorPage from './pages/EditorPage';
+import { ThemeProvider } from './context/ThemeContext';
 
 function App() {
 
   return (
     <>
-    <EditorPage />
+    <ThemeProvider>
+      <EditorPage />
+    </ThemeProvider>
     </>
   )
 }
