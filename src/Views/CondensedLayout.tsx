cb5efe97c176aa8ab5210b95b1c270abaa09569f
Bernardo <bernardo_rego@hotmail.com>
2025-04-13 14:38:37 -0300

    STYLE: Modos de Visualização.

---
+++ src/Views/CondensedLayout.tsx
@@ -0,0 +1,73 @@
+import { Col, Container, Row } from "react-bootstrap";
+import PersonalHeader from "../components/PersonalHeader";
+import SupernaturalTemplateSelect from "../components/SupernaturalTemplateSelect";
+import PersonalInfoSection from "../components/sections/PersonalInfoSection";
+import AttributeSection from "../components/sections/AttributesSection";
+import SkillsSection from "../components/sections/SkillsSection";
+import MeritsSection from "../components/sections/MeritsSection";
+import OtherTraitsSection from "../components/sections/OtherTraitsSection";
+import EquipmentsSection from "../components/sections/EquipmentsSection";
+import CharacterNavbar from "../components/CharacterNavbar";
+import HealthSection from "../components/sections/HealthSection";
+import WillpowerSection from "../components/sections/WillpowerSection";
+import MoralitySection from "../components/sections/MoralitySection";
+import ConditionsSection from "../components/sections/ConditionsSection";
+import AspirationsSections from "../components/sections/AspirationsSection";
+import GallerySection from "../pages/GallerySection";
+
+export default function CondensedLayout() {
+  return (
+    <Container fluid className="mt-4">
+      {/* Cabeçalho do personagem */}
+      <CharacterNavbar />
+      <SupernaturalTemplateSelect />
+      <PersonalHeader />
+
+      {/* Atributos (3 colunas lado a lado) */}
+      <Row className="mt-3">
+        <Col><AttributeSection /></Col>
+      </Row>
+
+      {/* Habilidades + Méritos + Vantagens Derivadas */}
+      <Row className="mt-3">
+        <Col md={12}><SkillsSection orientation="horizontal" /></Col>
+      </Row>
+
+      {/* Informações pessoais (duas colunas pequenas) */}
+      <Row className="mt-3">
+        <Col md={6}> <HealthSection />   </Col>
+        <Col md={6}> <WillpowerSection /> </Col>
+        <Col md={6}>
+          {/* Condições */}
+          <ConditionsSection />
+    
+          {/* Aspirações */}
+          <AspirationsSections />
+        </Col>
+        <Col md={6}>
+          <MoralitySection />
+        </Col>
+      </Row>
+
+      <Row className="mt-3">
+        <Col md={6}><MeritsSection /></Col>
+        <Col md={6}><OtherTraitsSection /></Col>
+      </Row>
+
+      {/* Equipamentos: Ataques, Armaduras, Gerais */}
+      <Row className="mt-3">
+        <Col><EquipmentsSection /></Col>
+      </Row>
+
+      {/* Descrição física e história */}
+      <Row className="mt-3">
+        <Col> <PersonalInfoSection /> </Col>
+      </Row>
+      
+      {/* Galeria */}
+      <Row className="mt-3">
+        <Col> <GallerySection /> </Col>
+      </Row>
+    </Container>
+  );
+}
---
+++ src/Views/OriginalLayout.tsx
@@ -0,0 +1,57 @@
+import { Col, Container, Row } from "react-bootstrap";
+import CharacterNavbar from "../components/CharacterNavbar";
+import SupernaturalTemplateSelect from "../components/SupernaturalTemplateSelect";
+import PersonalHeader from "../components/PersonalHeader";
+import AttributeSection from "../components/sections/AttributesSection";
+import SkillsSection from "../components/sections/SkillsSection";
+import MeritsSection from "../components/sections/MeritsSection";
+import OtherTraitsSection from "../components/sections/OtherTraitsSection";
+import EquipmentsSection from "../components/sections/EquipmentsSection";
+import PersonalInfoSection from "../components/sections/PersonalInfoSection";
+import StatusPanel from "../components/sections/StatusPanel";
+import GallerySection from "../pages/GallerySection";
+
+export default function OriginalLayout() {
+  
+  return (
+    <>
+      <CharacterNavbar />
+      <Container className="mt-4" fluid>
+        <SupernaturalTemplateSelect />
+        <PersonalHeader />
+        <AttributeSection />
+
+        <Row className="mt-4">
+          {/* Coluna de Habilidades */}
+          <Col md={4}> 
+            <SkillsSection /> 
+          </Col>
+          
+          {/* Todo o resto */}
+          <Col md={8}>
+            <Row>
+              {/* Vantagens */}
+              <Col md={7}> 
+                <MeritsSection />
+                <OtherTraitsSection />
+              </Col>
+              <Col md={5}> <StatusPanel /> </Col>
+            </Row>
+
+            {/* Seção de Equipamentos */}
+            <Row>
+              <Col> <EquipmentsSection /> </Col>
+            </Row>
+          </Col>
+          
+        </Row>
+      </Container>
+      
+      <div className="p-2">
+        <PersonalInfoSection />
+        <GallerySection />
+      </div> 
+     
+    </>
+  );
+}
