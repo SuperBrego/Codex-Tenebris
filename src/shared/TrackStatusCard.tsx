65b9e8ea70c9aa487050a01af418b5f6577f060a
Bernardo <bernardo_rego@hotmail.com>
2025-04-13 17:16:45 -0300

    Subtítulo

--- src/shared/TrackStatusCard.tsx
+++ src/shared/TrackStatusCard.tsx
@@ -9,11 +9,12 @@ interface BaseProps {
   boxes: HealthBox[];
   label: string;
   max?: number;
+  subtitle?: string;
 }
 
 type Props = | (BaseProps & { trait: string; templateTrait?: never }) | (BaseProps & { trait?: never; templateTrait: string });
 
-export default function TrackStatusCard({ boxes, max = 30, label, trait, templateTrait }: Props): ReactElement {
+export default function TrackStatusCard({ boxes, max = 30, label, trait, templateTrait, subtitle }: Props): ReactElement {
   const { character, updateCharacter } = useCharacter();
   const { colors } = useTheme();
 
@@ -30,8 +31,9 @@ export default function TrackStatusCard({ boxes, max = 30, label, trait, templat
       <Card.Header style={{ backgroundColor: colors.primary, color: colors.primaryText }}> 
         <span className="fw-bold">{label}</span>
       </Card.Header>
-      <Card.Body className="d-flex justify-content-center">
+      <Card.Body className="d-flex flex-column justify-content-center text-center">
         <StatusBoxesList traitBoxes={boxes} max={max} onChange={handleChangeStats} />
+        {(subtitle && subtitle.length > 0) && <div className="mt-2 text-muted"><small>{subtitle}</small></div>}
       </Card.Body>
     </Card>
   </>;
