be0db0dca3a309a9502d7124c2fc87774ab76d64
Bernardo <bernardo_rego@hotmail.com>
2025-04-20 18:00:29 -0300

    FEAT: Adicionado fundo no cabeçalho.

--- src/components/SupernaturalTemplateSelect.tsx
+++ src/components/SupernaturalTemplateSelect.tsx
@@ -12,15 +12,27 @@ const TEMPLATE_LABELS: Record<SupernaturalTemplatesIDs, string> = {
   [SupernaturalTemplatesIDs.Deviant]: 'deviant',
 };
 
+const BACKGROUNDS: Record<SupernaturalTemplatesIDs, string> = {
+  [SupernaturalTemplatesIDs.Mortal]: '/headers/mortal_header.png',
+  [SupernaturalTemplatesIDs.Vampire]: '/headers/vampire_header.png',
+  [SupernaturalTemplatesIDs.Werewolf]: '/headers/werewolf_header.png',
+  [SupernaturalTemplatesIDs.Deviant]: 'deviant',
+};
+
 export default function SupernaturalTemplateSelect() {
   const { character, updateCharacter } = useCharacter();
   const { t } = useTranslation();
 
   return (
     <header 
-      className="p-3 border-bottom rounded-bottom" 
+      className="p-5 border-bottom rounded-bottom" 
+      style={{ 
+        backgroundImage: `url(${BACKGROUNDS[character.template]})`, 
+        backgroundSize: '100% 300%', 
+        backgroundPosition: 'center' 
+      }}
     >
-      <Form.Label className="fw-semibold"> {t('templateSelect')} </Form.Label>
+      <Form.Label className="fw-semibold h3"> {t('templateSelect')} </Form.Label>
       <Form.Select
         id={`template-select-${character.template}`}
         value={character.template}
